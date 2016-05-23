2b32d42d0ac32a1366159c41b7d662602e33bb89
Caption-He <912772051@qq.com>
2016-05-23 21:09:25 +0800

    改善天气API

--- views/tianqi.php
+++ views/tianqi.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>天气查询</title>
-   <?php
-    error_reporting(0); 
-   $nam = $_POST[cityname]; 
-      class tianqi
-   {
-    public function qi($name)
-    {
-       
+<?php
+	error_reporting(0);
+	$name = $_POST[cityname];
+	
     $ch = curl_init();
-    $url = "http://apis.baidu.com/apistore/weatherservice/cityname?cityname=$name";
+    $url = $url = "http://apis.baidu.com/apistore/weatherservice/cityname?cityname=".$name;
     $header = array(
         'apikey:59cea956bc89f0429cc0b331b4e9f12d',
     );
@@ -21,19 +13,24 @@
     // 执行HTTP请求
     curl_setopt($ch , CURLOPT_URL , $url);
     $res = curl_exec($ch);
-    $city = json_decode($res);    
-        return $city;
-        }
-    }
-   $sas = new tianqi();
-   $re = $sas->qi($nam);
+    $json = json_decode($res);
 
+    echo "城市名称：".$json->{'retData'}->city;
+    echo '<br>';
+    echo "查询日期：".$json->{'retData'}->date;
+    echo '<br>';
+    echo "查询时间：".$json->{'retData'}->time;
+    echo '<br>';
+    echo "天气状况：".$json->{'retData'}->weather;
+    echo '<br>';
+    echo "最高气温：".$json->{'retData'}->l_tmp;
+    echo '<br>';
+    echo "最低气温：".$json->{'retData'}->h_tmp;
+    echo '<br>';
+    echo "风况：".$json->{'retData'}->WD;
+    echo $json->{'retData'}->WS;
+    echo '<br>';
+    echo "日出时间：".$json->{'retData'}->sunrise;
+    echo '<br>';
+    echo "日落时间：".$json->{'retData'}->sunset;
 ?>
-</head>
-<body>
-<?php
-var_dump($re);
-
-?>
-</body>
-</html>
